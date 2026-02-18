362cbc7ec90c78b0ffe181ca116902dc37ec8695
**123456Aa <Ahmeday5>
2026-02-18 16:54:01 +0200

    edit login page

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -9,16 +9,38 @@ import { AuthService } from '../../../../core/services/auth.service';
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   email = '';
   password = '';
+  showPassword = false;
+  rememberMe = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
 
   login() {
     this.authService.login(this.email, this.password);
-    this.router.navigate(['/leads']);  // بعد اللوجين، روح للـ Leads (الـ Guard هيحدد الشكل)
+    this.router.navigate(['/leads']); // بعد اللوجين، روح للـ Leads (الـ Guard هيحدد الشكل)
+  }
+
+  quickLogin(role: string) {
+    const accounts: any = {
+      admin: { email: 'admin@admin.com', pass: 'admin123' },
+      marketing: { email: 'marketing@marketing.com', pass: 'marketing123' },
+      sales: { email: 'sales@sales.com', pass: 'sales123' },
+      support: { email: 'support@support.com', pass: 'support123' },
+      developer: { email: 'developer@developer.com', pass: 'dev123' },
+    };
+
+    this.email = accounts[role].email;
+    this.password = accounts[role].pass;
   }
 }
